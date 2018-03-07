b142e71d9a53f799db1640f0400e75da2998f435
Cundong Zhang <155466@qq.com>
2018-03-07 14:14:03 +0800

    添加下游的途损判断方式

--- src/services/bkmSettle.service.js
+++ src/services/bkmSettle.service.js
@@ -21,10 +21,12 @@
 
             var v = angular.extend({}, settleParams);
 
+            v.downLossWay = (v.downLossWay == null) ? v.lossWay : v.downLossWay;
+
             //下游免责途损数量: 按比例时，转换为每车的免责数量
-            v.downLossRangeQuant = v.lossWay == 1 ? v.downLossRange * v.loaded : v.downLossRange;
+            v.downLossRangeQuant = v.downLossWay == 1 ? v.downLossRange * v.loaded : v.downLossRange;
             //下游亏吨扣款: (亏吨数量-免责途损数量) * 货品单价, 如果未设置途损，则不扣款
-            v.downstreamLossAmount = (v.losses - (v.lossWay == 0 ? v.losses : v.downLossRangeQuant)) * (v.goodsUnitPrice || 0);
+            v.downstreamLossAmount = (v.losses - (v.downLossWay == 0 ? v.losses : v.downLossRangeQuant)) * (v.goodsUnitPrice || 0);
             v.downstreamLossAmount = v.downstreamLossAmount > 0 ? v.downstreamLossAmount : 0;
             //下游结算数量: 判断下游结算数量策略
             v.downstreamFinalWeight = !v.downFinalWeightPolicy ? v.loaded : (v.downFinalWeightPolicy == 1 ? v.receipt : Math.min(v.loaded, v.receipt));
