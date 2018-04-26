af0ba8fce9b377e856538e7af07abf1f4926ef5e
Cundong Zhang <155466@qq.com>
2018-04-26 20:03:25 +0800

    下游结算小数点四舍五入

--- src/services/bkmSettle.service.js
+++ src/services/bkmSettle.service.js
@@ -24,6 +24,7 @@
             //下游亏吨扣款: (亏吨数量-免责途损数量) * 货品单价, 如果未设置途损，则不扣款
             v.downstreamLossAmount = (v.losses - (v.downLossWay == 0 ? v.losses : v.downLossRangeQuant)) * (v.goodsUnitPrice || 0);
             v.downstreamLossAmount = v.downstreamLossAmount > 0 ? v.downstreamLossAmount : 0;
+            v.downstreamLossAmount = Math.round(v.downstreamLossAmount * 100) / 100;
             //下游结算数量: 判断下游结算数量策略
             v.downstreamFinalWeight = !v.downFinalWeightPolicy ? v.loaded : (v.downFinalWeightPolicy == 1 ? v.receipt : Math.min(v.loaded, v.receipt));
             //下游结算数量增减
@@ -34,16 +35,17 @@
             //下游裸运价
             v.noDownTaxedFreightPrice = v.isDownIncludeTax ? v.freightPrice * (1 - v.downTaxRate) : v.freightPrice;
             //下游原始运费金额：结算数量 * 运费单价
-            v.freightAmount = v.downstreamFinalWeight * v.freightPrice;
+            v.freightAmount = Math.round(v.downstreamFinalWeight * v.freightPrice * 100) / 100;
             //下游含税运费金额：结算数量 * 含税运费单价
-            v.taxedFreightAmount = v.downstreamFinalWeight * v.downTaxedFreightPrice;
+            v.taxedFreightAmount = Math.round(v.downstreamFinalWeight * v.downTaxedFreightPrice * 100) / 100;
             //下游裸运费金额：结算数量 * 裸运费单价
-            v.noTaxedFreightAmount = v.downstreamFinalWeight * v.noDownTaxedFreightPrice;
+            v.noTaxedFreightAmount = Math.round(v.downstreamFinalWeight * v.noDownTaxedFreightPrice * 100) / 100;
             //未含税结算金额:  抹零取整（裸运费金额 - 亏吨扣款 + + 运费增减 ），去掉个位（包含）数后的零头
             var finalAmount = v.noTaxedFreightAmount - v.downstreamLossAmount + (v.downAmountAdjust || 0);
             v.downNoTaxedFinalAmount = (v.isIgnoreSmall ? parseInt(finalAmount / 10) * 10 : finalAmount);
             //下游含税结算金额
             v.downstreamFinalAmount = v.downNoTaxedFinalAmount / (1 - v.downTaxRate);
+            v.downstreamFinalAmount = Math.round(v.downstreamFinalAmount * 100) / 100;
             //下游税费
             v.downTaxedAmount = v.downstreamFinalAmount - v.downNoTaxedFinalAmount;
 
