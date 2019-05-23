1ed73e2576ca130d6c753be550be21796e92a475
Cundong Zhang <155466@qq.com>
2019-05-23 18:08:21 +0800

    增加浏览器所在操作系统的判断

--- src/util/bkmUtil.js
+++ src/util/bkmUtil.js
@@ -891,4 +891,25 @@ bkm.browser = function() {
     return {
         s: _browser
     };
+
+
 }();
+
+// 判断操作系统
+bkm.os = (function () {
+    var UserAgent = navigator.userAgent.toLowerCase();
+    return {
+        isIpad: /ipad/.test(UserAgent),
+        isIphone: /iphone os/.test(UserAgent),
+        isAndroid: /android/.test(UserAgent),
+        isWindowsCe: /windows ce/.test(UserAgent),
+        isWindowsMobile: /windows mobile/.test(UserAgent),
+        isWin2K: /windows nt 5.0/.test(UserAgent),
+        isXP: /windows nt 5.1/.test(UserAgent),
+        isVista: /windows nt 6.0/.test(UserAgent),
+        isWin7: /windows nt 6.1/.test(UserAgent),
+        isWin8: /windows nt 6.2/.test(UserAgent),
+        isWin81: /windows nt 6.3/.test(UserAgent),
+        isMac: /mac os/.test(UserAgent)
+    };
+}());
