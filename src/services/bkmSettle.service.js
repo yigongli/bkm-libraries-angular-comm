e1dd97a62ddf6c8e0a01b9661720dcd780f7e4d5
Cundong Zhang <155466@qq.com>
2020-03-27 13:51:18 +0800

    代码优化

--- src/services/bkmSettle.service.js
+++ src/services/bkmSettle.service.js
@@ -12,15 +12,15 @@ bkm.settle.downstreamSettlementComputing = function (settleParams) {
     v.loadUnloadAmount = (v.loadingPrice || 0) + (v.unloadingPrice || 0);
     v.downLossWay = (v.downLossWay == null) ? v.lossWay : v.downLossWay;
     //下游免责途损数量: 按比例时，转换为每车的免责数量
-    v.downLossRangeQuant = v.downLossWay == 1 ? v.downLossRange * v.loaded : v.downLossRange;
+    v.downLossRangeQuant = v.downLossWay === bkm.CST.LossWay_Rate ? v.downLossRange * v.loaded : v.downLossRange;
     v.downLossRangeQuant = bkm.util.round(v.downLossRangeQuant);
     //下游亏吨扣款: (亏吨数量-免责途损数量) * 货品单价, 如果未设置途损，则不扣款
     v.losses = bkm.util.round(v.losses);
-    v.downstreamLossAmount = (v.losses - (v.downLossWay == 0 ? v.losses : v.downLossRangeQuant)) * (v.goodsUnitPrice || 0);
+    v.downstreamLossAmount = (v.losses - (v.downLossWay === bkm.CST.LossWay_NotSet  ? v.losses : v.downLossRangeQuant)) * (v.goodsUnitPrice || 0);
     v.downstreamLossAmount = v.downstreamLossAmount > 0 ? v.downstreamLossAmount : 0;
     v.downstreamLossAmount = bkm.util.round(v.downstreamLossAmount) || 0;
     //下游结算数量: 判断下游结算数量策略
-    v.downstreamFinalWeight = !v.downFinalWeightPolicy ? v.loaded : (v.downFinalWeightPolicy == 1 ? v.receipt : Math.min(v.loaded, v.receipt));
+    v.downstreamFinalWeight = v.downFinalWeightPolicy === bkm.CST.FinalWeightPolicy_Loaded ? v.loaded : (v.downFinalWeightPolicy === bkm.CST.FinalWeightPolicy_Reception ? v.receipt : Math.min(v.loaded, v.receipt));
     //下游结算数量增减
     v.downstreamFinalWeight = v.downstreamFinalWeight + (v.downFinalWeightAdjust || 0);
     v.downstreamFinalWeight = bkm.util.round(v.downstreamFinalWeight);
